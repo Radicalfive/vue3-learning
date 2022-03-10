455efc0a765291e07316c097d3e5e7c4a1323dcf
Radicalfive <1561548288@qq.com>
2022-03-10 22:09:43 +0800

    vue

--- ES6常用语法复习/ const的⽤法.js
+++
@@ -1,4 +0,0 @@
-// const的用法
-const a = 5;
-// a = 10;
-console.log(a + 10);
--- ES6常用语法复习/Array.from()的⽤法.js
+++
@@ -1,6 +0,0 @@
-let playersObje = { '0': "curry", '1 ': "james", '2': "kobe", length: 3 };
-let players = Array.from(playersObje);
-console.log(players);  // ["curry","james","kobe"]
-let playersSet = new Set(['curry', 'james', 'curry'])
-players = Array.from(playersSet);
-console.log(players);  // ["curry", "james "],Set可以去重
